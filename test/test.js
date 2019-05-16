6e0e434505a1da0adfdbd1960d89303ef93b1533
Unknown <ruben.s.ruiz@outlook.com>
2019-05-16 11:48:45 -0500

    Implemented testing, comments and safeguards to prevent invalid data

---
+++ test/test.js
@@ -0,0 +1,12 @@
+/**
+ * Backend API Unit Tests Runner
+ * Please keep tests in a separate file 
+ * based on what they test and add them
+ * here
+ */
+
+const assert = require("assert"),
+      dataValidatorTest = require("./dataValidatorTest");
+
+dataValidatorTest.checkEmail();
+
