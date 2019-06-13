096a37f764bb64fd2b99b6d78dfbb8e17e7495a0
savannahrayegonzales <50592222+savannahrayegonzales@users.noreply.github.com>
2019-06-13 11:39:34 -0500

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+//add this file to .gitignore
+
+module.exports = {
+    google: {
+        clientID: "216440639844-2np27o3him2enojrt30g0m6o5l4uegs8.apps.googleusercontent.com",
+        clientSecret: "WO6-bMTsayp0Mz82O_N-LXfv"
+    },
+    mongodb: {
+        dbURI: "mongodb+srv://admin:test@passport-test-lbsec.mongodb.net/test?retryWrites=true&w=majority"
+    },
+    session: {
+        cookieKey: "anavocadothanks"
+    }
+};
