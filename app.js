76e50b322e7c5a2696ac08bbab48f2dedb515473
Andrew Ordonez <andrewordonez5@gmail.com>
2019-06-18 11:47:34 -0500

    makes it kinda work i hope

--- app.js
+++ app.js
@@ -4,22 +4,42 @@ const profileRoutes = require('./routes/profile-routes');
 const passportSetup = require('./config/passport-setup');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+var cookieParser = require("cookie-parser");
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const path =require('path');
+var cors = require("cors");
 
+
+                                                   
 const app = express();
 
 //set up view engine
+app.set("views", path.join(__dirname, "views"));
 app.set('view engine','ejs');
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
 
 app.use(cookieSession({
     maxAge: 24*60*60*1000,
     keys: [keys.session.cookieKey]
 }));
 
+
+
+
+
+
 //initialize passport
 app.use(passport.initialize());
 app.use(passport.session());
+
 
 //connect to mongodb
 mongoose.connect(keys.mongodb.dbURI, () => {
@@ -35,6 +55,23 @@ app.get('/',(req,res) => {
     res.render('home',{user: req.user});
 })
 
-app.listen(2000, () => {
-    console.log('app now listening for requests on port 2000');
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
 });
+
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+});
+
+// app.listen(2000, () => {
+//     console.log('app now listening for requests on port 2000');
+// });
+
+module.exports = app; 
