9c9c3e04a86987dfc7d297db954254344da0eced
savannahrayegonzales <50592222+savannahrayegonzales@users.noreply.github.com>
2019-06-19 10:24:22 -0500

    Update user-model.js

--- models/user-model.js
+++ models/user-model.js
@@ -4,9 +4,50 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     username: String,
     googleId: String,
-    thumbnail: String
+});
+
+const boardSchema = new Schema({
+    name: String,
+    boardId: String,
+    backgroundImg: String,
+    boardUsers: [userSchema],
+    swimlanes: {
+        title: String,
+        id: String,
+        cards: {
+            title: String,
+            description: String,
+            id: String,
+            comments: [{
+                comment: String,
+                id: String,
+            }],
+            attachments: [{
+                location: String,
+                id: String,
+            }],
+            users: [{
+                name: String,
+                id: String,
+            }],
+            labels: [{
+                color: String,
+                title: String,
+                id: String,
+            }],
+            dueDates: Date,
+        },
+    },
+    labels: [{
+        color: String,
+        title: String,
+    }]
+
+
 });
 
 const User = mongoose.model('user', userSchema);
+const Board = mongoose.model('board', boardSchema);
 
 module.exports = User;
+module.exports = Board;
